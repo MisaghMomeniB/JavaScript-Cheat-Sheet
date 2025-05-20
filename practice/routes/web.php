11596338ecac0c4b24bbcf94cd10119765774630
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-20 10:13:53 +0330

    update

--- practice/routes/web.php
+++ practice/routes/web.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
 
+Route::get('/login', [PageController::class, 'login']);
 
+Route::get('/register', [PageController::class, 'register'])->middleware('guest', 'throtte:10,1');
 
 ?>
