c10127a050cc1f54c03a284e705120032e517331
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-24 20:14:22 +0330

    uddate

--- ticket_system/app/Http/Controllers/TicketController.php
+++ ticket_system/app/Http/Controllers/TicketController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class TicketController extends Controller
 {
-    //
+    public function index()
+        {
+            $tickets = auth()->user()->tickets;
+            return view('tickets.index', compact('tickets'));
+        }
+
+        public function create()
+        {
+            return view('tickets.create');
+        }
+
+        public function store(Request $request)
+        {
+            $request->validate([
+                'subject' => 'required',
+                'message' => 'required',
+            ]);
+
+            $ticket = auth()->user()->tickets()->create($request->only('subject', 'message'));
+
+            return redirect()->route('tickets.show', $ticket);
+        }
+
+        public function show(Ticket $ticket)
+        {
+            $this->authorize('view', $ticket); // در صورت نیاز
+
+            return view('tickets.show', compact('ticket'));
+        }
+
 }
