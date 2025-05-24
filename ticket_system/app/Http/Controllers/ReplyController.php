8201956d019114dfe4a83ae2bcf3089a6431a44d
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-24 20:15:04 +0330

    update

--- ticket_system/app/Http/Controllers/ReplyController.php
+++ ticket_system/app/Http/Controllers/ReplyController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class ReplyController extends Controller
 {
-    //
+    public function store(Request $request, Ticket $ticket)
+        {
+            $request->validate([
+                'message' => 'required',
+            ]);
+
+            $ticket->replies()->create([
+                'user_id' => auth()->id(),
+                'message' => $request->message,
+            ]);
+
+            return back();
+        }
+
 }
