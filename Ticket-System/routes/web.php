a63a070ec58303b8ebb082a078426a6338bb9490
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-24 10:58:43 +0330

    update

--- Ticket-System/routes/web.php
+++ Ticket-System/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [PageController::class, ('login')]);
+Route::get('/login', function() {
+    return view('login');
+});
