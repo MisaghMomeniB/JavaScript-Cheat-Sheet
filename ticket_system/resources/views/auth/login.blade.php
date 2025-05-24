e0f803199a6137da3a781d7e28ecb70d778fb987
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-24 20:10:28 +0330

    login page

---
+++ ticket_system/resources/views/auth/login.blade.php
@@ -0,0 +1,8 @@
+<h2>Login</h2>
+<form method="POST" action="{{ route('login') }}">
+    @csrf
+    <input name="email" type="email" placeholder="Email" required><br>
+    <input name="password" type="password" placeholder="Password" required><br>
+    <button type="submit">Login</button>
+</form>
+<a href="{{ route('register') }}">Register</a>
