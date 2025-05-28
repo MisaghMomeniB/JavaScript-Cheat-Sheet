0a7efa11edbead6ca0f80ee88d84502c3bd56dfd
MisaghMomeniB <jenabemisagh@gmail.com>
2025-05-28 12:04:26 +0330

    create controller and setup

--- ticket_system/app/Http/Controllers/UserController.php
+++ ticket_system/app/Http/Controllers/UserController.php
@@ -2,9 +2,93 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return User::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'username' => 'required|string|max:20|unique:users, username',
+            'phone_number' => 'required|string|max:11|unique:users, username',
+            'password' => 'required|string|min:6|max:18',
+        ]);
+
+        $user = User::create([
+            'username' => $validated['username'],
+            'phone_number' => $validated['phone_number'],
+            'password' => $validated['password'],
+        ]);
+
+        return response()->json($user, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return User::findOrFail();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $user = User::findOrFail($id);
+
+        $validated = $request->validate([
+            'username' => 'sometimes|string|max:20|unique:users, username',
+            'phone_number' => 'sometimes|string|size:11|unique:users, username',
+            'password' => 'sometimes|string|min:6|max:18',
+        ]);
+
+        if(isset($validated['password'])) {
+            $user->password = $validated['password'];
+        }
+
+        $user->update($validated);
+
+        return response()->json($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted']);
+    }
 }
