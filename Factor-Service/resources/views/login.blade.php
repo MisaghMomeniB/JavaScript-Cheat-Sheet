96cf91f0aecaaea1b68515fb2871c71cf7b696fa
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-01 16:25:02 +0330

    set tailwind css link and base code html

--- Factor-Service/resources/views/login.blade.php
+++ Factor-Service/resources/views/login.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <!-- People find pleasure in different ways. I find it in keeping my mind clear. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Login</title>
+</head>
+<body>
+    
+</body>
+</html>
