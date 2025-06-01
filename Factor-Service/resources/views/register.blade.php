7d033e510d0e200ee141d2ff338d5d9d3fce8c6e
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-01 16:21:03 +0330

    base code html

--- Factor-Service/resources/views/register.blade.php
+++ Factor-Service/resources/views/register.blade.php
@@ -1,3 +1,13 @@
-<div>
-    <!-- The biggest battle is the war against ignorance. - Mustafa Kemal Atatürk -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Register</title>
+</head>
+<body>
+    
+</body>
+</html>
