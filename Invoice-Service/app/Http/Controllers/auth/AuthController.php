001d1c965555a8c7ab258f0ad5b536717e82ea87
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-03 11:57:33 +0330

    set route and set controller

--- Invoice-Service/app/Http/Controllers/auth/AuthController.php
+++ Invoice-Service/app/Http/Controllers/auth/AuthController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
+    // showLogin Controller and Set Route
     public function showLogin() {
         return view('form.login');
     }
 
+    // showRegister Controller and Set Route
     public function showRegister() {
         return view('/form.register');
     }
