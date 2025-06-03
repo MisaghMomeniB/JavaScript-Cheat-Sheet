c70c1daa53e15c6da1bf01be1ed0756f5c7eb6e9
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-03 15:35:39 +0330

    update and link css and input lable fo rinvice service

--- Invoice-Service/resources/views/invoice/invoice.blade.php
+++ Invoice-Service/resources/views/invoice/invoice.blade.php
@@ -4,24 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.fontcdn.ir/Font/Persian/Vazir/Vazir.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ثبت فاکتور</title>
 </head>
 <body dir="rtl" class="font-Vazir" >
-    <form class="text-Vazir" action="">
-        <label for="fname">فروشگاه</label>
-        <input type="text" id="fname" name="fname"><br>
-        <label for="lname">دسته بندی</label>
-        <select name="cars" id="cars">
-            <option value="volvo">1</option>
-            <option value="saab">2</option>
-            <option value="mercedes">3</option>
-            <option value="audi">4</option>
-        </select><br>
-        <label for="price">قیمت</label>
-        <input type="number" placeholder="ریال" name="price" id="price"><br>
-        <label for="description">توضیحات</label>
-         <input type="text" name="description" id="description">
+    <form class="text-Vazir m-10" action="">
+        <div>
+            <label for="fname">فروشگاه</label>
+            <input type="text" id="fname" name="fname"><br>
+            <label for="lname">دسته بندی</label>
+            <select name="cars" id="cars">
+                <option value="volvo">1</option>
+                <option value="saab">2</option>
+                <option value="mercedes">3</option>
+                <option value="audi">4</option>
+            </select><br>
+            <label for="price">قیمت</label>
+            <input type="number" placeholder="ریال" name="price" id="price"><br>
+            <label for="description">توضیحات</label>
+            <input type="text" name="description" id="description">
+        </div>
+
     </form>
 </body>
 </html>
