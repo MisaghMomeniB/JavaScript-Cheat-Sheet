dcfdfed433f3085c4cdb5f9a075e9dee5c4b6428
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-03 14:52:20 +0330

    update font size and family and structure for form

--- Invoice-Service/resources/views/invoice/invoice.blade.php
+++ Invoice-Service/resources/views/invoice/invoice.blade.php
@@ -3,10 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.fontcdn.ir/Font/Persian/Vazir/Vazir.css" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ثبت فاکتور</title>
 </head>
-<body>
-    
+<body dir="rtl" class="font-Vazir" >
+    <form class="text-Vazir" action="">
+        <label for="fname">فروشگاه</label>
+        <input type="text" id="fname" name="fname"><br>
+        <label for="lname">دسته بندی</label>
+        <select name="cars" id="cars">
+            <option value="volvo">1</option>
+            <option value="saab">2</option>
+            <option value="mercedes">3</option>
+            <option value="audi">4</option>
+        </select><br>
+        <label for="price">قیمت</label>
+        <input type="number" placeholder="ریال" name="price" id="price"><br>
+        <label for="description">توضیحات</label>
+         <input type="text" name="description" id="description">
+    </form>
 </body>
 </html>
