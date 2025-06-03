a98e8b8146b9be0979c8205906cca0dcbf189827
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-03 11:55:48 +0330

    define auth controller and route web

---
+++ Invoice-Service/app/Http/Controllers/auth/AuthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function showLogin() {
+        return view('form.login');
+    }
+
+    public function showRegister() {
+        return view('/form.register');
+    }
+}
--- Invoice-Service/routes/web.php
+++ Invoice-Service/routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/register', function() {
-    return view('form.register');
-});
+// Route::get('/register', function() {
+//     return view('form.register');
+// });
 
-Route::get('/login', function() {
-    return view('form.login');
-});
+Route::get('/register', [AuthController::class, 'showRegister']);
+
+// Route::get('/login', function() {
+//     return view('form.login');
+// });
+
+Route::get('/login', [AuthController::class, 'showLogin']);
