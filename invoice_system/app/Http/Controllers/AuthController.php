86afdd3ddd37d081251a8345470610f7aadd9e09
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-05 21:14:27 +0330

    set/config models

--- invoice_system/app/Http/Controllers/AuthController.php
+++ invoice_system/app/Http/Controllers/AuthController.php
@@ -2,9 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+public function showLogin() {
+return view('auth.login');
+}
 
+public function login(Request $request) {
+if (Auth::attempt($request->only('email', 'password'))) {
+return redirect()->route('dashboard');
+}
+return back()->withErrors(['email' => 'اطلاعات اشتباه است']);
+}
+
+public function logout() {
+Auth::logout();
+return redirect()->route('login');
+}
+
+public function showRegister() {
+return view('auth.register');
+}
+
+public function register(Request $request) {
+$request->validate([
+'name' => 'required',
+'email' => 'required|email|unique:users',
+'password' => 'required|confirmed',
+]);
+
+User::create([
+'name' => $request->name,
+'email' => $request->email,
+'password' => Hash::make($request->password),
+]);
+
+return redirect()->route('login');
+}
 }
--- invoice_system/app/Http/Controllers/InvoiceController.php
+++ invoice_system/app/Http/Controllers/InvoiceController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Invoice;
+use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
 
 class InvoiceController extends Controller
 {
-    //
+public function index() {
+$invoices = Auth::user()->invoices;
+return view('dashboard', [
+'invoices' => $invoices,
+'categories' => Category::all()
+]);
+}
+
+public function store(Request $request) {
+$request->validate([
+'store' => 'required',
+'date' => 'required|date',
+'category_id' => 'required|exists:categories,id',
+'price' => 'required|integer',
+]);
+
+Auth::user()->invoices()->create($request->all());
+return redirect()->route('dashboard');
+}
 }
