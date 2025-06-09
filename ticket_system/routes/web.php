20016568152874da36351d1511e528dd24e25fab
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-09 11:57:49 +0330

    define register and showregister route for get and post method

--- ticket_system/routes/web.php
+++ ticket_system/routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/register', [AuthController::class, 'showRegister']);
+Route::post('/register', [AuthController::class, 'register']);
