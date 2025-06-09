7c12cefe97025896f918e84ce7d93a71d3d6948b
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-09 11:58:59 +0330

    define middleware and tickets route

--- ticket_system/routes/web.php
+++ ticket_system/routes/web.php
@@ -11,3 +11,20 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth.custom')->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/dashboard', function () {
+        return redirect('/tickets');
+    });
+
+    Route::get('/tickets', [TicketController::class, 'index']);
+    Route::get('/tickets/create', [TicketController::class, 'create']);
+    Route::post('/tickets', [TicketController::class, 'store']);
+
+    Route::middleware('role:admin')->group(function () {
+        Route::get('/tickets/{id}/edit', [TicketController::class, 'edit']);
+        Route::put('/tickets/{id}', [TicketController::class, 'update']);
+    });
+});
