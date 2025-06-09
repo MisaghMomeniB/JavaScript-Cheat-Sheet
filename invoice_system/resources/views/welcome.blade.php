c0abf90797af88d953e2cbb94a57bfbbee3c6604
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-09 11:51:03 +0330

    update welcome page

--- invoice_system/resources/views/welcome.blade.php
+++ invoice_system/resources/views/welcome.blade.php
@@ -1,13 +1,24 @@
-<dov>
-    <style>
-        body {
-            font-size: 30px;
-            text-align: center;
-            text-decoration: none;
-            padding: 20px;
-        }
-    </style>
-    <a href="login">login Page</a>
-    <br>
-    <a style="padding-top: 12px" href="register">Register Page</a>
-</dov>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="hero bg-base-200 min-h-screen">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hello there</h1>
+      <p class="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+        quasi. In deleniti eaque aut repudiandae et a id nisi.
+      </p>
+      <a href="login"><button class="btn btn-primary">Login</button></a>
+    </div>
+  </div>
+</div>
+</body>
+</html>
