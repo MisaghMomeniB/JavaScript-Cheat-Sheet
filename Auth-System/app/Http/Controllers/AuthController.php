014375623f91309d76d73ef79a4bd44c9ebb6070
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-11 17:04:07 +0330

    set and conf controller for showRegister and showLogin

--- Auth-System/app/Http/Controllers/AuthController.php
+++ Auth-System/app/Http/Controllers/AuthController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    // LoginGet
+    public function showLogin() {
+        return view('auth.login');
+    }
+
+    // RegisterGet
+    public function showRegister() {
+        return view('auth.register');
+    }
 }
