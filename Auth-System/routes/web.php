197bb2cf83225e9f086b161de8879d7ba3b1c2c9
MisaghMomeniB <jenabemisagh@gmail.com>
2025-06-11 17:05:05 +0330

    create and conf routes

--- Auth-System/routes/web.php
+++ Auth-System/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// LoginGet and Post
+Route::get('/login', [AuthController::class, 'showLogin'])->name('showLogin');
+
+// RegisterGet and Post
+Route::get('/register', [AuthController::class, 'showRegister'])->name('showRegister');
